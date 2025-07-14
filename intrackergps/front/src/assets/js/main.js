9d46f3a958aaa86b4fe893cb8d76428df17c806c
Jairo Martínez <jairomarti@gmail.com>
2025-07-14 09:17:59 -0400

    Cambios minimos en owl carrousel

--- intrackergps/front/src/assets/js/main.js
+++ intrackergps/front/src/assets/js/main.js
@@ -1,20 +1,16 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
-    }
-})
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 4,
+        autoplay: false,
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        navText: ["<i class='fa fa-long-arrow-left'>", "<i class='fa fa-long-arrow-right'>"]
+    })
+
+    $('.owl-carousel').find('.owl-nav').removeClass('disabled');
+        $('.owl-carousel').on('changed.owl.carousel', (event) =>{
+            $(this).find('.owl-nav').removeClass('disabled');
+    });
+});
